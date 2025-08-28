45baa14922143fa8176df6b3758db6661edb8c3b
Baljinder Singh <baljinder.saluja@gmail.com>
2025-08-28 13:20:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // If deploying to GitHub Pages under a repo name, set base to '/<repo>/'.
-  base: '/skillshub/'
+  base: '/skillshub/dist/'
 })
